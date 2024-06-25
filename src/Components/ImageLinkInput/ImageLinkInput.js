ac5f7e57c8772a742f00e4aca7fa7a170ab121e7
dayana.allisonn <dayana.allisonn@gmail.com>
2024-06-25 13:06:31 +0300

    FaceDetectionSection
    This section adds multiple face detection feature to the app, using react classes.

--- src/Components/ImageLinkInput/ImageLinkInput.js
+++ src/Components/ImageLinkInput/ImageLinkInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkInput.css';
 
-const ImageLinkInput = () => {
+const ImageLinkInput = ({onInputChange, onButtonClicked}) => {
     return (
         <div>
           <p className='f3'>
@@ -9,8 +9,8 @@ const ImageLinkInput = () => {
           </p>
           <div className='center'>
               <div className='form pa4 br3 shadow-5 center'>
-                  <input className='f4 pa2 w-70 center' type='text' />
-                  <button className='f4 w-30 grow link ph3 pv2 dib white bg-orange pointer'>Detect</button>
+                  <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+                  <button className='f4 w-30 grow link ph3 pv2 dib white bg-orange pointer' onClick={onButtonClicked}>Detect</button>
               </div>
           </div>
         </div>
